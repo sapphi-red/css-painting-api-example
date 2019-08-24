9e0f651233577f01ea0d798b2572f260529023db
sapphi-red <green@sapphi.red>
2019-08-25 05:59:10 +0900

    fix

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ CSS.paintWorklet.addModule("./paintworklet.js")
 const $display = document.getElementById("display")
 const $url = document.getElementById("url")
 const $set = document.getElementById("set")
+const $wset = document.getElementById("wset")
 
 $set.addEventListener("click", () => {
+  $display.style.listStyleImage = `url(${$url.value})`
+})
+
+$wset.addEventListener("click", async () => {
+  const $img = new Image()
+  $img.src = $url.value
+  await $img.decode()
   $display.style.listStyleImage = `url(${$url.value})`
 })
