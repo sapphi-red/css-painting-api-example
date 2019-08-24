10b9849859bc522cb77bb97f3f4642763d45d32f
sapphi-red <green@sapphi.red>
2019-08-25 05:31:58 +0900

    change

--- paintworklet.js
+++ paintworklet.js
@@ -9,6 +9,7 @@ registerPaint(
       const w2 = width / 2
       const h2 = height / 2
       ctx.drawImage(img, 0, 0, w2, h2)
+      ctx.filter = "blur(2px)"
       ctx.drawImage(img, w2, h2, w2, h2)
     }
   }
